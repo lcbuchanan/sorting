624563bdca24c9dba7a175e57e76076dc807aa79
lcbuchanan <buchanan.lori@gmail.com>
2017-11-02 15:06:33 -0500

    making some tweaks

--- mergesort.js
+++ mergesort.js
@@ -1,14 +1,10 @@
 function mergeSort(arr) {
-    if (arr.length === 1 || arr.length === 0) return arr;
-
+    if (arr.length < 2) return arr;
     var twoArrays = split(arr);
     return merge(mergeSort(twoArrays[0]), mergeSort(twoArrays[1]));
 }
 
 function merge(arr1, arr2) {
-    if (!arr1.length) return arr2;
-    if (!arr2.length) return arr1;
-
     var output = [];
     var i = 0;
     var j = 0;
@@ -21,12 +17,11 @@ function merge(arr1, arr2) {
             j++;
         }
     }
-
     return output;
 }
 
 function split(arr) {
-    var firstHalf = arr.slice(0, Math.floor(arr.length / 2));
-    var secondHalf = arr.slice(Math.floor(arr.length / 2));
+    var firstHalf = arr.slice(0, (arr.length / 2));
+    var secondHalf = arr.slice((arr.length / 2));
     return [firstHalf, secondHalf];
 }
